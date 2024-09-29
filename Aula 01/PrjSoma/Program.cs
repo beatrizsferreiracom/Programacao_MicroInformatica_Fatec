3c33911bde1ef7ce9262743c48ffa9138d716db0
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-09-29 11:53:16 -0300

    Add files via upload

---
+++ Aula 01/PrjSoma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjSoma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar variáveis
+            double N1 = 0, N2 = 0, R = 0;
+            Console.WriteLine("Informe um número:");
+            N1 = Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Informe outro número:");
+            N2 = Convert.ToDouble(Console.ReadLine());
+            R = N1 + N2;
+            Console.Write(N1 + " + " + N2 + " = " + R.ToString());
+            Console.ReadKey();
+        }
+    }
+}
