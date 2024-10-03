91bc918b78d1ddb3fbbaad6af5c46fa42c2a3d70
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-10-03 08:42:35 -0300

    Add files via upload

---
+++ Aula 05/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace CalculoImc
+{
+    partial class CalculoImc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimparListas = new System.Windows.Forms.Button();
+            this.lblM = new System.Windows.Forms.Label();
+            this.lblH = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lstM = new System.Windows.Forms.ListBox();
+            this.lstH = new System.Windows.Forms.ListBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.cmbSexo = new System.Windows.Forms.ComboBox();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLimparListas
+            // 
+            this.btnLimparListas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnLimparListas.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnLimparListas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimparListas.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimparListas.ForeColor = System.Drawing.Color.White;
+            this.btnLimparListas.Location = new System.Drawing.Point(445, 403);
+            this.btnLimparListas.Name = "btnLimparListas";
+            this.btnLimparListas.Size = new System.Drawing.Size(185, 42);
+            this.btnLimparListas.TabIndex = 39;
+            this.btnLimparListas.Text = "LIMPAR LISTAS";
+            this.btnLimparListas.UseVisualStyleBackColor = false;
+            this.btnLimparListas.Click += new System.EventHandler(this.btnLimparListas_Click);
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblM.ForeColor = System.Drawing.Color.White;
+            this.lblM.Location = new System.Drawing.Point(480, 219);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(106, 23);
+            this.lblM.TabIndex = 38;
+            this.lblM.Text = "MULHERES";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblH.ForeColor = System.Drawing.Color.White;
+            this.lblH.Location = new System.Drawing.Point(484, 31);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(94, 23);
+            this.lblH.TabIndex = 37;
+            this.lblH.Text = "HOMENS";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(121, 380);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(121, 48);
+            this.btnSair.TabIndex = 35;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(196, 306);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(121, 48);
+            this.btnLimpar.TabIndex = 34;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lstM
+            // 
+            this.lstM.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstM.FormattingEnabled = true;
+            this.lstM.ItemHeight = 20;
+            this.lstM.Location = new System.Drawing.Point(386, 248);
+            this.lstM.Name = "lstM";
+            this.lstM.Size = new System.Drawing.Size(295, 144);
+            this.lstM.TabIndex = 32;
+            this.lstM.SelectedIndexChanged += new System.EventHandler(this.listM_SelectedIndexChanged);
+            // 
+            // lstH
+            // 
+            this.lstH.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstH.FormattingEnabled = true;
+            this.lstH.ItemHeight = 20;
+            this.lstH.Location = new System.Drawing.Point(386, 63);
+            this.lstH.Name = "lstH";
+            this.lstH.Size = new System.Drawing.Size(295, 144);
+            this.lstH.TabIndex = 31;
+            this.lstH.SelectedIndexChanged += new System.EventHandler(this.ListH_SelectedIndexChanged);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.lblPeso.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.ForeColor = System.Drawing.Color.White;
+            this.lblPeso.Location = new System.Drawing.Point(23, 168);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(87, 34);
+            this.lblPeso.TabIndex = 26;
+            this.lblPeso.Text = "Peso:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(136, 173);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(198, 32);
+            this.txtPeso.TabIndex = 25;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.lblAltura.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.ForeColor = System.Drawing.Color.White;
+            this.lblAltura.Location = new System.Drawing.Point(23, 105);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(103, 34);
+            this.lblAltura.TabIndex = 24;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(136, 110);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(198, 32);
+            this.txtAltura.TabIndex = 23;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.lblNome.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(23, 41);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(106, 34);
+            this.lblNome.TabIndex = 22;
+            this.lblNome.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(136, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(198, 32);
+            this.txtNome.TabIndex = 21;
+            // 
+            // cmbSexo
+            // 
+            this.cmbSexo.AutoCompleteCustomSource.AddRange(new string[] {
+            "Masculino",
+            "Feminino"});
+            this.cmbSexo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbSexo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSexo.FormattingEnabled = true;
+            this.cmbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.cmbSexo.Location = new System.Drawing.Point(136, 230);
+            this.cmbSexo.Name = "cmbSexo";
+            this.cmbSexo.Size = new System.Drawing.Size(198, 31);
+            this.cmbSexo.TabIndex = 40;
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.lblSexo.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSexo.ForeColor = System.Drawing.Color.White;
+            this.lblSexo.Location = new System.Drawing.Point(23, 225);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(90, 34);
+            this.lblSexo.TabIndex = 41;
+            this.lblSexo.Text = "Sexo:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(45, 306);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(121, 48);
+            this.btnCalcular.TabIndex = 42;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // CalculoImc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(707, 466);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.cmbSexo);
+            this.Controls.Add(this.btnLimparListas);
+            this.Controls.Add(this.lblM);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstM);
+            this.Controls.Add(this.lstH);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.Name = "CalculoImc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLimparListas;
+        private System.Windows.Forms.Label lblM;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lstM;
+        private System.Windows.Forms.ListBox lstH;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ComboBox cmbSexo;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Aula 05/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculoImc
+{
+    public partial class CalculoImc : Form
+    {
+        public CalculoImc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtAltura.Text = "";
+            txtPeso.Text = "";
+            cmbSexo.Text = "";
+        }
+
+        private void btnLimparListas_Click(object sender, EventArgs e)
+        {
+            lstM.Items.Clear();
+            lstH.Items.Clear();
+        }
+
+        private void ListH_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nome, categoria;
+                double altura, peso, imc;
+                nome = txtNome.Text;
+                altura = double.Parse(txtAltura.Text);
+                peso = double.Parse(txtPeso.Text);
+                imc = Math.Round(peso / (altura * altura), 2);
+
+                if (imc < 18.5) categoria = "Abaixo do peso";
+
+                else if (imc >= 18.5 && imc < 25) categoria = "Normal";
+
+                else if (imc >= 25 && imc < 30) categoria = "Sobrepeso";
+
+                else categoria = "Obesidade";
+
+                if (cmbSexo.Text == "Masculino")
+                {
+                    lstH.Items.Add($"{nome} - IMC: {imc} - {categoria}");
+                }
+                else if (cmbSexo.Text == "Feminino")
+                {
+                    lstM.Items.Add($"{nome} - IMC: {imc} - {categoria}");
+                }
+                else
+                {
+                    MessageBox.Show("Escolha um sexo", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Informe os valores numéricos", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show($"Ocorreu um erro: {erro.Message}", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+    }
+}
