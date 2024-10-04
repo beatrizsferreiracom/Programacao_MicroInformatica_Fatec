c2ef6769c0548ab17440d73c162a3e33615f223d
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-10-04 17:10:43 -0300

    Add files via upload

---
+++ Aula 06/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Tabuada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVisualizar = new System.Windows.Forms.Button();
+            this.lblResiultado = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lstT = new System.Windows.Forms.ListBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVisualizar
+            // 
+            this.btnVisualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnVisualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVisualizar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVisualizar.ForeColor = System.Drawing.Color.White;
+            this.btnVisualizar.Location = new System.Drawing.Point(85, 193);
+            this.btnVisualizar.Name = "btnVisualizar";
+            this.btnVisualizar.Size = new System.Drawing.Size(197, 67);
+            this.btnVisualizar.TabIndex = 58;
+            this.btnVisualizar.Text = "VISUALIZAR";
+            this.btnVisualizar.UseVisualStyleBackColor = false;
+            this.btnVisualizar.Click += new System.EventHandler(this.btnVisualizar_Click);
+            // 
+            // lblResiultado
+            // 
+            this.lblResiultado.AutoSize = true;
+            this.lblResiultado.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResiultado.ForeColor = System.Drawing.Color.White;
+            this.lblResiultado.Location = new System.Drawing.Point(446, 32);
+            this.lblResiultado.Name = "lblResiultado";
+            this.lblResiultado.Size = new System.Drawing.Size(118, 23);
+            this.lblResiultado.TabIndex = 53;
+            this.lblResiultado.Text = "RESULTADO";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(85, 366);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(197, 67);
+            this.btnSair.TabIndex = 52;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(151)))), ((int)(((byte)(142)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(85, 278);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(197, 67);
+            this.btnLimpar.TabIndex = 51;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lstT
+            // 
+            this.lstT.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstT.FormattingEnabled = true;
+            this.lstT.ItemHeight = 27;
+            this.lstT.Location = new System.Drawing.Point(398, 64);
+            this.lstT.Name = "lstT";
+            this.lstT.Size = new System.Drawing.Size(222, 355);
+            this.lstT.TabIndex = 49;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.lblNum.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.ForeColor = System.Drawing.Color.White;
+            this.lblNum.Location = new System.Drawing.Point(27, 53);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(323, 40);
+            this.lblNum.TabIndex = 44;
+            this.lblNum.Text = "Informe o número:";
+            this.lblNum.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(50, 106);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(272, 48);
+            this.txtNum.TabIndex = 43;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(181)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(657, 465);
+            this.Controls.Add(this.btnVisualizar);
+            this.Controls.Add(this.lblResiultado);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstT);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TABUADA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVisualizar;
+        private System.Windows.Forms.Label lblResiultado;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lstT;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.TextBox txtNum;
+    }
+}
+
---
+++ Aula 06/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada
+{
+    public partial class Form1 : Form
+    {
+        int i, n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblNome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AcceptButton = btnVisualizar;
+            CancelButton = btnLimpar;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNum.Text = "";
+            lstT.Items.Clear();
+        }
+
+        private void btnVisualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = int.Parse(txtNum.Text);
+                for (i = 1; i <= 10; i++)
+                {
+                    lstT.Items.Add($"{n} x {i} = {n*i}");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Informe o valor", "Tabuada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show($"Ocorreu um erro: {erro.Message}", "Tabuada", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
